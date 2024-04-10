5b77706494b6050d21a67dba308dcbaee3894807
Kyle Lu <nbnb6712@gmail.com>
2024-04-10 19:40:18 +0800

    fix: create project

---
+++ chongAiTa/chongAiTa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chongAiTa
+//
+//  Created by Kyle Lu on 2024/4/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
