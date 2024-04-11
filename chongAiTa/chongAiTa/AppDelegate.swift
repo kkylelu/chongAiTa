3d6ec68d25212a3802af44c2d473e9697a5743ce
Kyle Lu <nbnb6712@gmail.com>
2024-04-11 14:57:54 +0800

    docs: add google service plist

--- chongAiTa/chongAiTa/AppDelegate.swift
+++ chongAiTa/chongAiTa/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 import IQKeyboardManagerSwift
 
 @main
@@ -14,9 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         
         IQKeyboardManager.shared.enable = true
+        FirebaseApp.configure()
         
         return true
     }
