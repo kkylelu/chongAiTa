c139c2a71c9d58cf82baf9a10e79e1f8b68d87f7
Kyle Lu <nbnb6712@gmail.com>
2024-04-17 10:59:44 +0800

    fix: chatGPT answer separate the text into paragraphs

--- chongAiTa/chongAiTa/Network/TextGenerationManager.swift
+++ chongAiTa/chongAiTa/Network/TextGenerationManager.swift
@@ -15,7 +15,7 @@ class TextGenerationManager {
         let urlEndpoint = OpenAINetworkHelper.shared.baseURL + "chat/completions"
         let journalTexts = journals.map { $0.body }.joined(separator: "\n")
         let messages = [
-            ["role": "system", "content": "You are a professional pet diary summarizer. Please, with a warm and friendly tone, help pet owners reflect on their pets' diary entries from this month and transform these records into a complete short story using the pet owner's voice. In the story, use vague terms like 'one day' or 'one evening' instead of specific times to broaden the story's universality. Ensure that the story is complete within a 200 token limit, with no cliffhangers or unfinished sentences. The story should include a clear beginning, development, and conclusion to ensure semantic completeness. Please respond in Traditional Chinese (Taiwan)."],
+            ["role": "system", "content": "You are a professional pet diary summarizer. Please, with a warm and friendly tone, help pet owners reflect on their pets' diary entries from this month and transform these records into a complete short story using the pet owner's voice. In the story, use vague terms like 'one day' or 'one evening' instead of specific times to broaden the story's universality. Ensure that the story is complete within a 200 token limit, with no cliffhangers or unfinished sentences. The story should include a clear beginning, development, and conclusion to ensure semantic completeness. Please separate the text into paragraphs with blank lines between them. Please respond in Traditional Chinese (Taiwan)."],
             ["role": "user", "content": journalTexts]
         ]
         let parameters: Parameters = [
