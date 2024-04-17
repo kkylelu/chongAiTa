f157e0671defd33e932f3c2066280e28b17cdedf
Kyle Lu <nbnb6712@gmail.com>
2024-04-17 17:00:59 +0800

    fix: mapView layout

--- chongAiTa/chongAiTa/Controller/MapViewController.swift
+++ chongAiTa/chongAiTa/Controller/MapViewController.swift
@@ -10,22 +10,31 @@ import GoogleMaps
 
 class MapViewController: UIViewController {
     
+    var mapView: GMSMapView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupUI()
     }
-    
-    override func loadView() {
+
+    func setupUI() {
         let camera = GMSCameraPosition.camera(withLatitude: 25.039413072140746, longitude: 121.53243457301599, zoom: 16.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-        view = mapView
-        
+
+        mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(mapView)
+
+        NSLayoutConstraint.activate([
+            mapView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            mapView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
+            mapView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor)
+        ])
+
         let marker = GMSMarker()
         marker.position = CLLocationCoordinate2D(latitude: 25.039413072140746, longitude: 121.53243457301599)
         marker.title = "AppWorks School"
         marker.snippet = "100台北市中正區仁愛路二段99號"
         marker.map = mapView
-        
     }
-    
 }
