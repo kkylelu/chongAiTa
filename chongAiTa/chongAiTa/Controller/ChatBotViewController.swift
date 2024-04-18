7f69d7d2bc592ff8e381fc0ed8d4b103c8bb3c83
Kyle Lu <nbnb6712@gmail.com>
2024-04-18 14:32:57 +0800

    fix: talk to chatbot but in one tableView cell

--- chongAiTa/chongAiTa/Controller/ChatBotViewController.swift
+++ chongAiTa/chongAiTa/Controller/ChatBotViewController.swift
@@ -8,77 +8,97 @@
 import UIKit
 
 class ChatBotViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITextFieldDelegate {
-
+    
     var tableView: UITableView!
-        var textField: UITextField!
-        var sendButton: UIButton!
-
-    var messages: [String] = ["123"]
+    var textField: UITextField!
+    var sendButton: UIButton!
+    
+    var messages: [String] = []
     
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         setupUI()
-
+        
     }
-
+    
     //MARK: - Setup UI
     func setupUI() {
-            tableView = UITableView()
-            tableView.translatesAutoresizingMaskIntoConstraints = false
-            tableView.register(UITableViewCell.self, forCellReuseIdentifier: "messageCell")
-            tableView.dataSource = self
-            tableView.delegate = self
-            view.addSubview(tableView)
+        tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "messageCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+        view.addSubview(tableView)
+        
+        textField = UITextField()
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        view.addSubview(textField)
+        
+        sendButton = UIButton(type: .system)
+        sendButton.translatesAutoresizingMaskIntoConstraints = false
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
+        view.addSubview(sendButton)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: textField.topAnchor),
             
-            textField = UITextField()
-            textField.translatesAutoresizingMaskIntoConstraints = false
-            textField.borderStyle = .roundedRect
-            view.addSubview(textField)
+            textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            textField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            textField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -20),
             
-            sendButton = UIButton(type: .system)
-            sendButton.translatesAutoresizingMaskIntoConstraints = false
-            sendButton.setTitle("Send", for: .normal)
-            sendButton.addTarget(self, action: #selector(sendMessage), for: .touchUpInside)
-            view.addSubview(sendButton)
-            
-            NSLayoutConstraint.activate([
-                tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-                tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-                tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-                tableView.bottomAnchor.constraint(equalTo: textField.topAnchor),
-                
-                textField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-                textField.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-                textField.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: -20),
-                
-                sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-                sendButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-                sendButton.widthAnchor.constraint(equalToConstant: 80)
-            ])
-        }
+            sendButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            sendButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            sendButton.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
     
     // MARK: - Action
+    
     @objc func sendMessage() {
-            if let text = textField.text, !text.isEmpty {
-                messages.append(text)
-                let indexPath = IndexPath(row: messages.count - 1, section: 0)
-                tableView.insertRows(at: [indexPath], with: .automatic)
-                tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
-                textField.text = ""
+        guard let text = textField.text, !text.isEmpty else { return }
+        appendMessageAndReload("User: \(text)")
+        textField.text = ""
+        
+        ChatBotManager.shared.sendChatMessage(message: text) { [weak self] result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let response):
+                    self?.appendMessageAndReload("AI: \(response)")
+                case .failure(let error):
+                    self?.messages.append("Error: \(error.localizedDescription)")
+                    self?.appendMessageAndReload("Error: \(error.localizedDescription)")
+                }
             }
         }
+    }
+    
+    func appendMessageAndReload(_ message: String) {
+        messages.append(message)
+        let indexPath = IndexPath(row: messages.count - 1, section: 0)
+        tableView.beginUpdates()
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        tableView.endUpdates()
+        tableView.scrollToRow(at: indexPath, at: .bottom, animated: true)
+    }
+    
+    
     
     // MARK: - TableView Delegate
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            return messages.count
-        }
-        
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
-            cell.textLabel?.text = messages[indexPath.row]
-            return cell
-        }
-
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
+        cell.textLabel?.text = messages[indexPath.row]
+        return cell
+    }
+    
 }
