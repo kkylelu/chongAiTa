746a5cdc584d454984cc15ca1a4014e1b6d2ba38
Kyle Lu <nbnb6712@gmail.com>
2024-04-19 19:26:51 +0800

    fix: add navigation bar and DatPicker to choose year/month

--- chongAiTa/chongAiTa/Controller/CalendarViewController.swift
+++ chongAiTa/chongAiTa/Controller/CalendarViewController.swift
@@ -11,7 +11,7 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     
     let weekHeaderView = UIView()
-       let weekDays = ["一", "二", "三", "四", "五", "六", "日"]
+    let weekDays = ["一", "二", "三", "四", "五", "六", "日"]
     var collectionView: UICollectionView!
     var currentMonthDate = Date()
     
@@ -24,13 +24,12 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
     
     var firstWeekdayOfMonth: Int {
         let calendar = Calendar.current
-        // 週日為一週的第一天，如果不是，可以適當調整
         var components = calendar.dateComponents([.year, .month], from: currentMonthDate)
         components.day = 1
         let firstDayOfMonthDate = calendar.date(from: components)!
         let firstWeekday = calendar.component(.weekday, from: firstDayOfMonthDate)
         
-        // 將星期天作為索引0，若將星期一作為索引0，可以再減1
+        // 設定工作日第一天為星期一
         return firstWeekday - 2
     }
     
@@ -41,7 +40,7 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
         super.viewDidLoad()
         setupUI()
         configureCollectionView()
-        
+        setupNavigationBar()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -62,16 +61,16 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
         
         weekHeaderView.translatesAutoresizingMaskIntoConstraints = false
         weekHeaderView.backgroundColor = .systemGray6
-
+        
         view.addSubview(weekHeaderView)
         view.addSubview(collectionView)
-
+        
         NSLayoutConstraint.activate([
             weekHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             weekHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             weekHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             weekHeaderView.heightAnchor.constraint(equalToConstant: 20),
-    
+            
             collectionView.topAnchor.constraint(equalTo: weekHeaderView.bottomAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
@@ -102,35 +101,122 @@ class CalendarViewController: UIViewController, UICollectionViewDelegate, UIColl
     }
     
     func configureFlowLayout() {
-            guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
-            let itemWidth = collectionView.bounds.width / 7
-            let itemHeight = collectionView.bounds.height / 6
-            layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
-            layout.minimumInteritemSpacing = 0
-            layout.minimumLineSpacing = 0
-            layout.invalidateLayout()
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { return }
+        let itemWidth = collectionView.bounds.width / 7
+        let itemHeight = collectionView.bounds.height / 6
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        layout.invalidateLayout()
+    }
+    
+    func setupNavigationBar() {
+        let previousMonthButton = UIBarButtonItem(title: "<", style: .plain, target: self, action: #selector(goToPreviousMonth))
+        let nextMonthButton = UIBarButtonItem(title: ">", style: .plain, target: self, action: #selector(goToNextMonth))
+        
+        let titleButton = UIButton(type: .system)
+        titleButton.setTitle(titleForCurrentMonth(), for: .normal)
+        titleButton.addTarget(self, action: #selector(presentDatePicker), for: .touchUpInside)
+        
+        navigationItem.leftBarButtonItem = previousMonthButton
+        navigationItem.rightBarButtonItem = nextMonthButton
+        navigationItem.titleView = titleButton // 將 UIButton 設置為 titleView
+    }
+    
+    @objc func presentDatePicker() {
+        let datePicker = UIDatePicker()
+        datePicker.datePickerMode = .date
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.addTarget(self, action: #selector(datePickerDidChange(_:)), for: .valueChanged)
+        
+        var dateComponents = DateComponents()
+        dateComponents.year = Calendar.current.component(.year, from: Date())
+        dateComponents.month = Calendar.current.component(.month, from: Date())
+        let calendar = Calendar.current
+        datePicker.minimumDate = calendar.date(from: DateComponents(year: 2020))
+        datePicker.maximumDate = calendar.date(from: DateComponents(year: 2030))
+        
+        let alertController = UIAlertController(title: "\n\n\n\n\n\n\n\n\n", message: nil, preferredStyle: .actionSheet)
+        alertController.view.addSubview(datePicker)
+        
+        let selectAction = UIAlertAction(title: "選擇", style: .default, handler: { _ in
+            self.datePickerDidChange(datePicker)
+        })
+        
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        
+        alertController.addAction(selectAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @objc func datePickerDidChange(_ sender: UIDatePicker) {
+        currentMonthDate = sender.date
+        collectionView.reloadData()
+        updateTitleButton()
+    }
+    
+    
+    func updateTitleButton() {
+        if let titleButton = navigationItem.titleView as? UIButton {
+            titleButton.setTitle(titleForCurrentMonth(), for: .normal)
         }
+    }
+    
+    // MARK: - Action
+    
+    @objc func goToPreviousMonth() {
+        guard let prevMonthDate = Calendar.current.date(byAdding: .month, value: -1, to: currentMonthDate) else { return }
+        currentMonthDate = prevMonthDate
+        collectionView.reloadData()
+        updateTitleButton()
+    }
+    
+    @objc func goToNextMonth() {
+        guard let nextMonthDate = Calendar.current.date(byAdding: .month, value: 1, to: currentMonthDate) else { return }
+        currentMonthDate = nextMonthDate
+        collectionView.reloadData()
+        updateTitleButton()
+    }
+    
+    
+    func titleForCurrentMonth() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy 年 MM 月"
+        return formatter.string(from: currentMonthDate)
+    }
+    
     
     
     // MARK: - CollectionView Delegate
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // 加上月初前的空格
-        return 42 //daysInMonth + (firstWeekdayOfMonth - 1)
+        return 42
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DateCell", for: indexPath) as! DateCell
-        let date = Calendar.current.date(byAdding: .day, value: indexPath.item, to: Date()) // 生成日期假資料
-            let events = [
-                Event(title: "公園散步"),
-                Event(title: "打針"),
-                Event(title: "洗澡"),
-                Event(title: "看獸醫")
-            ]
-
-            cell.configureCell(with: date, events: events)
-            return cell
+        
+        // 已經是從 0 開始，不用再減 1
+        let firstDayIndex = firstWeekdayOfMonth
+        let dateOffset = indexPath.item - firstDayIndex
+        let calendar = Calendar.current
+        
+        if dateOffset >= 0 && dateOffset < daysInMonth {
+            let dateForCell = calendar.date(byAdding: .day, value: dateOffset, to: firstOfMonth())
+            cell.configureCell(with: dateForCell, events: [])
+        } else {
+            cell.configureCell(with: nil, events: [])
+        }
+        
+        return cell
+    }
+    
+    func firstOfMonth() -> Date {
+        let calendar = Calendar.current
+        let components = calendar.dateComponents([.year, .month], from: currentMonthDate)
+        return calendar.date(from: components)!
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
