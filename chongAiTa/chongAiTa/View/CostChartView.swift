8428fbd460733152e0cf67dae815a54be5c9c3e8
Kyle Lu <nbnb6712@gmail.com>
2024-04-23 10:28:54 +0800

    fix: show cost pie chart and list

--- chongAiTa/chongAiTa/View/CostChartView.swift
+++ chongAiTa/chongAiTa/View/CostChartView.swift
@@ -11,36 +11,50 @@ import SwiftUI
 struct CostChartView: View {
     @State private var costs: [(name: String, amount: Double)] = []
     @State private var totalCost: Double = 0
-    
+
     private func fetchData() {
         let allCosts = EventsManager.shared.getAllCosts()
         totalCost = allCosts.map { $0.cost }.reduce(0, +)
-
         costs = allCosts.map { cost in
             if let event = EventsManager.shared.loadEvent(with: cost.eventId) {
                 let activityName = event.activity.category.displayName
                 return (name: activityName, amount: cost.cost)
             } else {
-                return (name: "Unknown", amount: cost.cost)
+                return (name: "未分類", amount: cost.cost)
             }
         }
     }
-    
+
     var body: some View {
-        Chart {
-            ForEach(costs, id: \.name) { data in
+        List {
+            Chart(costs, id: \.name) { element in
                 SectorMark(
-                    angle: .value("Cost", data.amount / totalCost),
+                    angle: .value("Cost", element.amount),
                     innerRadius: .ratio(0.618),
                     angularInset: 1.5
                 )
                 .cornerRadius(5.0)
-                .foregroundStyle(by: .value("Activity", data.name))
-                .opacity(data.name == "Unknown" ? 0.3 : 1)
+                .foregroundStyle(by: .value("Activity", element.name))
+            }
+            .chartLegend(alignment: .center)
+            .scaledToFit()
+            .frame(maxWidth: .infinity)
+            .listRowSeparator(.hidden)
+            .frame(height: 300)
+
+            Section(header: Text("花費項目")) {
+                ForEach(costs, id: \.name) { cost in
+                    HStack {
+                        Text(cost.name)
+                        Spacer()
+                        Text("\(cost.amount / totalCost, specifier: "%.2f%%")").frame(alignment: .trailing)
+                        Spacer().frame(width: 20)
+                        Text("\(cost.amount, specifier: "$%.2f")").frame(alignment: .trailing)
+                    }
+                }
             }
         }
-        .chartLegend(alignment: .center, spacing: 18)
-        .scaledToFit()
+        .listStyle(.plain)
         .onAppear {
             fetchData()
         }
